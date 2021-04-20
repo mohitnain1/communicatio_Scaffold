65fbf282e2a810bc6a069c365a6d78508a4f5b74
pankaj-java-in <pankaj.java.in@gmail.com>
2021-04-20 19:13:49 +0530

    .

--- src/main/java/com/scaffold/chat/controller/WebSocketController.java
+++ src/main/java/com/scaffold/chat/controller/WebSocketController.java
@@ -10,7 +10,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.scaffold.chat.model.Message1;
 
