e6762531049283fd4e02409610525e70918f8010
AmitOodles1 <amit.mishra1@oodlestechnologies.com>
2021-05-26 15:25:48 +0530

    Unread Count | Non room members will not get chatroom data

--- src/main/java/com/scaffold/chat/ws/event/SubscriptionEventHandler.java
+++ src/main/java/com/scaffold/chat/ws/event/SubscriptionEventHandler.java
@@ -1,5 +1,12 @@
 package com.scaffold.chat.ws.event;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.time.LocalDateTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -8,18 +15,56 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 
+import com.scaffold.chat.domains.Member;
+import com.scaffold.chat.repository.ChatRoomRepository;
+import com.scaffold.chat.service.impl.UserServiceImpl;
 import com.scaffold.web.util.Destinations;
 
 @Component
 public class SubscriptionEventHandler implements ApplicationListener<SessionSubscribeEvent> {
 
 	@Autowired SimpMessagingTemplate template;
+	@Autowired ChatRoomRepository chatRoomRepository;
+	@Autowired UserServiceImpl userService;
+	
+	private static final Logger log = LoggerFactory.getLogger(SubscriptionEventHandler.class);
 	
 	@Override
 	public void onApplicationEvent(SessionSubscribeEvent event) {
 		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
 		UsernamePasswordAuthenticationToken user = (UsernamePasswordAuthenticationToken) headerAccessor.getUser();
-		resolveDestinationAndNotifyUsers(headerAccessor, user);
+		try {
+			resolveDestinationAndNotifyUsers(headerAccessor, user);
+			userLastSeenInRoom(user, headerAccessor);
+		}catch(Exception e) {
+			log.error("Exception Occured while user last seen updation or notifying subscription event with "
+					+ "localised message {}", e.getLocalizedMessage());
+		}
+	}
+
+	private void userLastSeenInRoom(UsernamePasswordAuthenticationToken user, StompHeaderAccessor headerAccessor) {
+		CompletableFuture.supplyAsync(() -> {
+			String email = user.getPrincipal().toString();
+			long userId = userService.loadUserByEmail(email).getUserId();
+			System.out.println("Current UserId : " + userId);
+			if(headerAccessor.getDestination().startsWith("/topic/conversations")) {
+				String chatRoomId = headerAccessor.getDestination().replace("/topic/conversations.", "");
+				chatRoomRepository.findByChatRoomIdAndIsDeleted(chatRoomId, false).map(room -> {
+					List<Member> members = room.getMembers().stream().map(member -> {
+						if (member.getUserId().equals(userId)) {
+							member.setLastSeen(LocalDateTime.now());
+							return member;
+						}
+						return member;
+					}).collect(Collectors.toList());
+					System.out.println("members : " + members.toString());
+					room.setMembers(members);
+					chatRoomRepository.save(room);
+					return true;
+				});
+			}
+			return false;
+		});
 	}
 
 	private void resolveDestinationAndNotifyUsers(StompHeaderAccessor headerAccessor, UsernamePasswordAuthenticationToken user) {
