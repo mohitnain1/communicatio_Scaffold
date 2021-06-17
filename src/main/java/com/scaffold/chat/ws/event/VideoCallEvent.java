599156242b48e461afffc85ab2bba3a3f4da815d
AmitOodles1 <amit.mishra1@oodlestechnologies.com>
2021-06-17 19:09:33 +0530

    Abc

--- src/main/java/com/scaffold/chat/ws/event/VideoCallEvent.java
+++ src/main/java/com/scaffold/chat/ws/event/VideoCallEvent.java
@@ -26,155 +26,163 @@ import com.scaffold.web.util.MessageEnum;
 
 @Component
 public class VideoCallEvent {
-	@Autowired public ChatRoomRepository chatRoomRepository;
-	@Autowired public MessageStoreRepository messageStoreRepository;
-	@Autowired public UserRepository userDetailsRepository;
-	@Autowired public SimpMessagingTemplate simpMessagingTemplate;
-	@Autowired MessageEventHandler messageEventHandler;
-	
-	public void incomingCall(Message<ChatPayload> message) {
+
+	@Autowired
+	public ChatRoomRepository chatRoomRepository;
+	@Autowired
+	public MessageStoreRepository messageStoreRepository;
+	@Autowired
+	public UserRepository userDetailsRepository;
+	@Autowired
+	public SimpMessagingTemplate simpMessagingTemplate;
+	@Autowired
+	MessageEventHandler messageEventHandler;
+
+	public void initiateCall(Message<Map<String, Object>> message) {
 		com.scaffold.chat.domains.Message savedMessage = messageEventHandler.saveMessage(getCallMessage(message),
 				messageEventHandler.getHeaderAccessor(message));
-		UserDataTransfer user = getUserBasicDetails(savedMessage.getSenderId());
-		Map<String, Object> response = new HashMap<String, Object>();	
-		if(Objects.nonNull(savedMessage)) {
-			response.put("id", savedMessage.getId());
-			response.put("sender", user);
-			response.put("sendingTime", savedMessage.getSendingTime());
-			response.put("content",savedMessage.getContent());
-			response.put("contentType", savedMessage.getContentType());
-			String chatRoomId = savedMessage.getDestination().replace("/app/call.", "");
-			if(Objects.nonNull(savedMessage)) {
-				System.out.println(savedMessage.getDestination());
-				String destination = String.format("/topic/conversations.", chatRoomId);
-				simpMessagingTemplate.convertAndSend(destination, response);
-			}
-		}
-		new Thread(() -> {incomingCallInvitation(savedMessage, user);}).start();
+		UserDataTransfer sender = getUserBasicDetails(savedMessage.getSenderId());
+		
+		
+		System.out.println("Saved message ________________________________________");
+		System.out.println("\n" + savedMessage.getContent());
+		Map<String, Object> response = new HashMap<String, Object>();
+		response.put("id", savedMessage.getId());
+		response.put("sender", sender);
+		response.put("sendingTime", savedMessage.getSendingTime());
+		response.put("content", savedMessage.getContent());
+		response.put("contentType", savedMessage.getContentType());
+		String chatRoomId = savedMessage.getDestination().replace("/app/call.", "");
+		String destination = String.format("/topic/conversations.", chatRoomId);
+		simpMessagingTemplate.convertAndSend(destination, response);
+		incomingCallInvitation(savedMessage, sender);
 	}
-	
+
 	public void incomingCallInvitation(com.scaffold.chat.domains.Message messagePayload, UserDataTransfer sender) {
 		Map<String, Object> response = new HashMap<String, Object>();
-		String chatRoomId=null;
-		if(messagePayload.getDestination().startsWith("/app/call")) {
-			chatRoomId= messagePayload.getDestination().replace("/app/call.", "");
+		String chatRoomId = null;
+		if (messagePayload.getDestination().startsWith("/app/call")) {
+			chatRoomId = messagePayload.getDestination().replace("/app/call.", "");
 			response.put("sender", sender);
-			response.put("chat", messagePayload);
 			response.put("totalMembers", getTotalMembers(chatRoomId));
 			response.put("inCallMembers", null);
 			response.put("chatRoomId", chatRoomId);
+			response.put("contentType", MessageEnum.INCOMING_CALL);
 		}
-		if(Objects.nonNull(chatRoomId)) {
-			chatRoomRepository.findByChatRoomIdAndIsDeleted(chatRoomId, false).ifPresent(chatRoom ->{				
-				List<Long> chatRoomMembersId = chatRoom.getMembers().stream().map(Member::getUserId)
+		if (Objects.nonNull(chatRoomId)) {
+			chatRoomRepository.findByChatRoomIdAndIsDeleted(chatRoomId, false).ifPresent(chatRoom -> {
+				List<Long> filteredMembers = chatRoom.getMembers().stream().map(Member::getUserId)
 						.filter(memberId -> !memberId.equals(sender.getUserId())).collect(Collectors.toList());
-				chatRoomMembersId.forEach(userId->{
+				for (Long memberId : filteredMembers) {
 					response.put("chatRoomName", chatRoom.getChatRoomName());
-					String destinationToNotify = String.format(Destinations.MESSAGE_EVENT_NOTIFICATION.getPath(), userId);
+					String destinationToNotify = String.format(Destinations.MESSAGE_EVENT_NOTIFICATION.getPath(),
+							memberId);
 					simpMessagingTemplate.convertAndSend(destinationToNotify, response);
-				});
+				}
 			});
 		}
 	}
-	
-	private ChatPayload getCallMessage(Message<ChatPayload> message) {
-		ChatPayload payload = message.getPayload();
-		if(payload.getContentType().equals(MessageEnum.CALL_INITIATED.getValue())) {
-			payload.setContentType(MessageEnum.INCOMING_CALL.getValue());
-			payload.setDestination(payload.getDestination());
-			payload.setSenderId(payload.getSenderId());
-			payload.setUsername(userDetailsRepository.findByUserId(payload.getSenderId()).getUsername());
-			payload.setContent("Call started by " + payload.getUsername());
-			payload.setSendingTime(new Date().getTime());
-			return payload;
-		}
-		return null;
+
+	private ChatPayload getCallMessage(Message<Map<String, Object>> message) {
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
+		Map<String, Object> payload = message.getPayload();
+		Long senderId = Long.parseLong(payload.get("userId").toString());
+		UserDataTransfer user = getUserBasicDetails(senderId);
+		return ChatPayload.builder().contentType(MessageEnum.INCOMING_CALL.getValue())
+					.destination(headerAccessor.getDestination())
+					.senderId(senderId).sendingTime(System.currentTimeMillis())
+					.content("Call Started by " +user.getUsername()).build();
 	}
 
 	public void callAccepted(Message<ChatPayload> message) {
 		ChatPayload chatPayload = message.getPayload();
-		Map<String, Object> response = new HashMap<String, Object>();	
-		if(Objects.nonNull(chatPayload.getSenderId())) {
+		Map<String, Object> response = new HashMap<String, Object>();
+		if (Objects.nonNull(chatPayload.getSenderId())) {
 			UserDataTransfer user = getUserBasicDetails(chatPayload.getSenderId());
 			response.put("sender", user);
 			response.put("sendingTime", new Date().getTime());
-			response.put("content", user.getUsername()+" joined.");
+			response.put("content", user.getUsername() + " joined.");
 			response.put("contentType", MessageEnum.CALL_ACCEPTED.getValue());
-			String destination = String.format(Destinations.VIDEO_CALL.getPath(), getHeaderAccessor(message).getDestination().replace("/app/call.", ""));
+			String destination = String.format(Destinations.VIDEO_CALL.getPath(),
+					getHeaderAccessor(message).getDestination().replace("/app/call.", ""));
 			simpMessagingTemplate.convertAndSend(destination, response);
 		}
 	}
-	
+
 	public void callRejected(Message<ChatPayload> message) {
 		ChatPayload chatPayload = message.getPayload();
-		Map<String, Object> response = new HashMap<String, Object>();	
-		if(Objects.nonNull(chatPayload.getSenderId())) {
+		Map<String, Object> response = new HashMap<String, Object>();
+		if (Objects.nonNull(chatPayload.getSenderId())) {
 			UserDataTransfer user = getUserBasicDetails(chatPayload.getSenderId());
 			response.put("sender", user);
 			response.put("sendingTime", new Date().getTime());
-			response.put("content", "Call declined by "+user.getUsername());
+			response.put("content", "Call declined by " + user.getUsername());
 			response.put("contentType", MessageEnum.CALL_REJECTED.getValue());
-			String destination = String.format(Destinations.VIDEO_CALL.getPath(), getHeaderAccessor(message).getDestination().replace("/app/call.", ""));
+			String destination = String.format(Destinations.VIDEO_CALL.getPath(),
+					getHeaderAccessor(message).getDestination().replace("/app/call.", ""));
 			simpMessagingTemplate.convertAndSend(destination, response);
 		}
 	}
-	
+
 	public void callDisconnected(Message<ChatPayload> message) {
 		ChatPayload chatPayload = message.getPayload();
-		Map<String, Object> response = new HashMap<String, Object>();	
-		if(Objects.nonNull(chatPayload.getSenderId())) {
+		Map<String, Object> response = new HashMap<String, Object>();
+		if (Objects.nonNull(chatPayload.getSenderId())) {
 			UserDataTransfer user = getUserBasicDetails(chatPayload.getSenderId());
 			response.put("sender", user);
 			response.put("sendingTime", new Date().getTime());
-			response.put("content", user.getUsername()+" left.");
+			response.put("content", user.getUsername() + " left.");
 			response.put("contentType", MessageEnum.CALL_REJECTED.getValue());
-			String destination = String.format(Destinations.VIDEO_CALL.getPath(), getHeaderAccessor(message).getDestination().replace("/app/call.", ""));
+			String destination = String.format(Destinations.VIDEO_CALL.getPath(),
+					getHeaderAccessor(message).getDestination().replace("/app/call.", ""));
 			simpMessagingTemplate.convertAndSend(destination, response);
 		}
 	}
-	
+
 	public void returnSignal(Message<SignalPayload> message) {
 		SignalPayload payload = message.getPayload();
-		Map<String, Object> response = new HashMap<String, Object>();	
-		if(Objects.nonNull(payload.getSenderId())) {
+		Map<String, Object> response = new HashMap<String, Object>();
+		if (Objects.nonNull(payload.getSenderId())) {
 			UserDataTransfer user = getUserBasicDetails(payload.getSenderId());
 			response.put("sender", user);
 			response.put("signal", payload.getSignal());
 			response.put("contentType", MessageEnum.RETURNING_SIGNAL.getValue());
-			String destination = String.format(Destinations.VIDEO_CALL.getPath(), getHeaderAccessor(message).getDestination().replace("/app/call.", ""));
+			String destination = String.format(Destinations.VIDEO_CALL.getPath(),
+					getHeaderAccessor(message).getDestination().replace("/app/call.", ""));
 			simpMessagingTemplate.convertAndSend(destination, response);
 		}
 	}
 
 	public void sendSignal(Message<SignalPayload> message) {
 		SignalPayload payload = message.getPayload();
-		Map<String, Object> response = new HashMap<String, Object>();	
-		if(Objects.nonNull(payload.getSenderId())) {
+		Map<String, Object> response = new HashMap<String, Object>();
+		if (Objects.nonNull(payload.getSenderId())) {
 			UserDataTransfer user = getUserBasicDetails(payload.getSenderId());
 			response.put("sender", user);
 			response.put("signal", payload.getSignal());
 			response.put("userToSignalId", payload.getUserToSignalId());
 			response.put("contentType", MessageEnum.SENDING_SIGNAL.getValue());
-			String destination = String.format(Destinations.VIDEO_CALL.getPath(), getHeaderAccessor(message).getDestination().replace("/app/call.", ""));
+			String destination = String.format(Destinations.VIDEO_CALL.getPath(),
+					getHeaderAccessor(message).getDestination().replace("/app/call.", ""));
 			simpMessagingTemplate.convertAndSend(destination, response);
 		}
 	}
-	
+
 	private List<UserDataTransfer> getTotalMembers(String chatRoomId) {
 		List<Member> members = chatRoomRepository.findByChatRoomIdAndIsDeleted(chatRoomId, false).get().getMembers();
 		return members.stream().map(memberInIt -> {
 			User user = userDetailsRepository.findByUserId(memberInIt.getUserId().longValue());
-			UserDataTransfer credentials = new UserDataTransfer(user.getUserId(), user.getImage(),user.getUsername());
+			UserDataTransfer credentials = new UserDataTransfer(user.getUserId(), user.getImage(), user.getUsername());
 			credentials.setIsCreator(memberInIt.isCreator());
 			credentials.setEmail(user.getEmail());
 			return credentials;
 		}).collect(Collectors.toList());
 	}
-	
+
 	public StompHeaderAccessor getHeaderAccessor(Message<?> message) {
 		return StompHeaderAccessor.wrap(message);
 	}
-	
+
 	private UserDataTransfer getUserBasicDetails(Long userId) {
 		return userDetailsRepository.findByUserId(userId)
 				.map(user -> new UserDataTransfer(user.getUserId(), user.getImage(), user.getUsername())).orElse(null);
