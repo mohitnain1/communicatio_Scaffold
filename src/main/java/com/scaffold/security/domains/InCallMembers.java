9874e51cfd8e66a45d5380fc7672b282c07cbebe
pankaj-java-in <pankaj.java.in@gmail.com>
2021-06-22 16:15:59 +0530

    Implement isCallActive Concept || Modification in inCallMembers.

---
+++ src/main/java/com/scaffold/security/domains/InCallMembers.java
@@ -0,0 +1,56 @@
+package com.scaffold.security.domains;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.scaffold.chat.datatransfer.UserDataTransfer;
+
+public class InCallMembers {
+	
+	private static InCallMembers INSTANCE;
+    
+    private InCallMembers() {        
+    }
+    
+    public static InCallMembers getInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new InCallMembers();
+        }
+        return INSTANCE;
+    }
+
+	private Map<String, List<UserDataTransfer>> inCallMembers = new ConcurrentHashMap<>();
+
+	public void setInCallMembers(String chatRoomId, UserDataTransfer member) {
+		if(inCallMembers.containsKey(chatRoomId)) {
+			List<UserDataTransfer> allActiveMembers = inCallMembers.get(chatRoomId);
+			if(!allActiveMembers.contains(member)) {
+				allActiveMembers.add(member);
+			}
+			inCallMembers.put(chatRoomId, allActiveMembers);
+		}else {
+			List<UserDataTransfer> allActiveMembers = new ArrayList<>();
+			allActiveMembers.add(member);
+			inCallMembers.put(chatRoomId, allActiveMembers);
+		}
+	}
+		
+	public List<UserDataTransfer> getInCallMembers(String chatRoomId) {
+		if(inCallMembers.containsKey(chatRoomId)) {
+			return inCallMembers.get(chatRoomId);
+		}
+		return null;
+	}
+	
+	public List<UserDataTransfer> removeInCallMembers(String chatRoomId, UserDataTransfer member) {
+		if(inCallMembers.containsKey(chatRoomId)) {
+			List<UserDataTransfer> allActiveMembers = inCallMembers.get(chatRoomId);
+			allActiveMembers.remove(member);
+			inCallMembers.put(chatRoomId, allActiveMembers);
+			return allActiveMembers;
+		}
+		return null;
+	}
+}
